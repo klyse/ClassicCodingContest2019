317cf151868a85c798e46414e0fc0fd7b25a996e
Unknown <Klaus.Pruenster@gmail.com>
2019-11-06 09:03:13 +0100

    updated second algo

--- Solver/Algorithms/Solver2.cs
+++ Solver/Algorithms/Solver2.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using System.Threading;
 using Solver.Model;
 
 namespace Solver.Algorithms
 {
-	public class Solver2 : ISolver<double, RoverInput2>
+	public class Position
 	{
-		public double Solve(RoverInput2 input)
+		public double X { get; set; }
+		public double Y { get; set; }
+		public double Angle { get; set; }
+
+		public Position(double x, double y, double angle)
 		{
-			var angle = input.Distance / input.TurnRadius;
+			X = x;
+			Y = y;
+			Angle = angle;
+		}
+
+		public override string ToString()
+		{
+			return $"{X:00} {Y:00} {Angle:00}";
+		}
+	}
+
+	public class Solver2 : ISolver<Position, RoverInput2>
+	{
+		public Position Solve(RoverInput2 input)
+		{
+			var alpha = Math.Round(input.Distance / input.TurnRadius, 2);
+
+			var x = Math.Round(input.TurnRadius / Math.Cos(alpha), 2);
+			var y = Math.Round(input.TurnRadius * Math.Sin(alpha), 2);
+
+			return new Position(x, y, 0);
 		}
 	}
 }
---
+++ Test/Solver2Test.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using Solver.Algorithms;
+using Solver.Model;
+
+namespace Test
+{
+	public class Solver2Test
+	{
+		[Test]
+		[TestCase("1,00 1,00 30,00", ExpectedResult = "12")]
+		public string T1(string inputStr)
+		{
+			var solver = new Solver2();
+			var input = new RoverInput2().Parse(inputStr.Split(' '));
+			var solution = solver.Solve(input);
+
+			return solution.ToString();
+		}
+	}
+}
