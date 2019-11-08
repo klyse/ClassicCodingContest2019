8468ba44dc7343efa13b9ce57dcc8145e1402dd1
Volkmar Rigo <volkmar@rigo.bz>
2019-11-08 17:13:16 +0100

    My Solver 3 - first draft

---
+++ Solver/Algorithms/Solver3v.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Solver.Base;
+using Solver.Model;
+using System.Drawing;
+
+namespace Solver.Algorithms
+{
+	public class Solver3v : ISolver<Output3, TerrainInput3>
+	{
+        public Output3 Solve(TerrainInput3 input)
+        {
+            var o = new Output3();
+            var countries = input.Matrix.GetFlat().Select(c => c.Country).Distinct().ToList();
+            countries.ForEach(c => o.Capital.Add(c, new Point { X = 0, Y = 0 }));
+
+            foreach (var country in countries)
+            {
+                var coords = new List<Point>();
+
+                input.Matrix.ExecuteOnAll((value, row, col) =>
+                {
+                    if (value.Country == country)
+                        coords.Add(new Point { X = col, Y = row });
+                });
+
+                var xs = coords.Select(q => q.X).Sum() / coords.Count;
+                var ys = coords.Select(q => q.Y).Sum() / coords.Count;
+
+                o.Capital[country] = new Point(xs, ys);
+            }
+
+            return o;
+        }
+	}
+}
