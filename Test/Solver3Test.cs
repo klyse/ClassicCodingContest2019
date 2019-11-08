d3550d6160219bd3b04ad1fdb0fc4bb0e608c696
Volkmar Rigo <volkmar@rigo.bz>
2019-11-08 16:19:18 +0100

    Test for solver 3

---
+++ Test/Solver3Test.cs
@@ -0,0 +1,29 @@
+using System;
+using NUnit.Framework;
+using Solver;
+using Solver.Algorithms;
+using Solver.Model;
+
+namespace Test {
+	public class Solver3Test
+	{
+		[Test]
+		[TestCase("Level3/level3_example.in")]
+		[TestCase("Level3/level3_1.in")]
+		[TestCase("Level3/level3_2.in")]
+		[TestCase("Level3/level3_3.in")]
+		[TestCase("Level3/level3_4.in")]
+		[TestCase("Level3/level3_5.in")]
+		public void T1(string file)
+		{
+			var fileInput = file.Read();
+			var input = new TerrainInput3().Parse(fileInput);
+
+			var solution = new Solver3().Solve(input);
+
+			file.Write(solution.ToString());
+			Console.WriteLine(solution);
+			Assert.Pass();
+		}
+	}
+}
