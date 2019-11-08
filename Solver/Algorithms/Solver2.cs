d7e566cc8242a7f93623705e3f6ac43cf99fbfb7
Volkmar Rigo <volkmar@rigo.bz>
2019-11-08 15:28:21 +0100

    Copy of Solver 1

--- Solver/Algorithms/Solver2.cs
+++ Solver/Algorithms/Solver2.cs
@@ -1,38 +1,21 @@
-﻿using System;
+﻿using System.Linq;
 using Solver.Base;
+using Solver.Model;
 
 namespace Solver.Algorithms
 {
-	public class Position
-	{
-		public double X { get; set; }
-		public double Y { get; set; }
-		public double Angle { get; set; }
+    public class Solver2 : ISolver<Output1, TerrainInput1>
+    {
+        public Output1 Solve(TerrainInput1 input)
+        {
+            var o = new Output1
+            {
+                Min = input.Data.GetFlat().Min(),
+                Max = input.Data.GetFlat().Max(),
+                Avg = (int)input.Data.GetFlat().Average()
+            };
 
-		public Position(double x, double y, double angle)
-		{
-			X = x;
-			Y = y;
-			Angle = angle;
-		}
-
-		public override string ToString()
-		{
-			return $"{X:00} {Y:00} {Angle:00}";
-		}
-	}
-
-	public class Solver2 : ISolver<Position, RoverInput2>
-	{
-		public Position Solve(RoverInput2 input)
-		{
-			var alpha = input.Distance / input.TurnRadius;
-
-
-			var x = Math.Round(input.TurnRadius * Math.Cos(alpha), 2);
-			var y = Math.Round(input.TurnRadius * Math.Sin(alpha), 2);
-
-			return new Position(x, y, 0);
-		}
-	}
+            return o;
+        }
+    }
 }
--- Test/Solver2Test.cs
+++ Test/Solver2Test.cs
@@ -1,27 +1,28 @@
 using NUnit.Framework;
 using Solver;
 using Solver.Algorithms;
+using Solver.Model;
 
 namespace Test
 {
-	public class Solver2Test
-	{
-		[Test]
-		[TestCase("Level1/level1_example.in")]
-		[TestCase("Level1/level1_1.in")]
-		[TestCase("Level1/level1_2.in")]
-		[TestCase("Level1/level1_3.in")]
-		[TestCase("Level1/level1_4.in")]
-		[TestCase("Level1/level1_5.in")]
-		public void T1(string file)
-		{
-			var fileInput = file.Read();
-			var input = new RoverInput1().Parse(fileInput);
+    public class Solver2Test
+    {
+        [Test]
+        [TestCase("Level1/level1_example.in")]
+        [TestCase("Level1/level1_1.in")]
+        [TestCase("Level1/level1_2.in")]
+        [TestCase("Level1/level1_3.in")]
+        [TestCase("Level1/level1_4.in")]
+        [TestCase("Level1/level1_5.in")]
+        public void T2(string file)
+        {
+            var fileInput = file.Read();
+            var input = new TerrainInput1().Parse(fileInput);
 
-			var solution = new Solver1().Solve(input);
+            var solution = new Solver1().Solve(input);
 
-			file.Write(solution.ToString());
-			Assert.Pass();
-		}
-	}
+            file.Write(solution.ToString());
+            Assert.Pass();
+        }
+    }
 }
