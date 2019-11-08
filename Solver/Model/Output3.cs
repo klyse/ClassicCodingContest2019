3ee47ef7b459c64d4456c6b720af8771d743b4ef
Unknown <Klaus.Pruenster@gmail.com>
2019-11-08 16:31:22 +0100

    fixed class

--- Solver/Model/Output3.cs
+++ Solver/Model/Output3.cs
@@ -1,24 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 
 namespace Solver.Model
 {
 	public class Output3
 	{
-        public class Location 
-        {
-            public int X { get; set; }
-            public int Y { get; set; }
-
-        }
-
-        public Dictionary<int, Location> Capital { get; set; } = new Dictionary<int, Location>();
+		public Dictionary<int, Point> Capital { get; set; } = new Dictionary<int, Point>();
 
 		public override string ToString()
 		{
 			return string.Join(Environment.NewLine, Capital.OrderBy(c => c.Key).Select(q => $"{q.Value.X} {q.Value.Y}"));
-
-        }
+		}
 	}
 }
