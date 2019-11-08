e0f79f5290602068a1f175a266403c00c9adc40e
Unknown <Klaus.Pruenster@gmail.com>
2019-11-08 15:52:48 +0100

    added solver 2 logic (at least a bit :D)

--- Solver/Algorithms/Solver2.cs
+++ Solver/Algorithms/Solver2.cs
@@ -9,6 +9,18 @@ namespace Solver.Algorithms
 		public Output2 Solve(TerrainInput2 input)
 		{
 			var o = new Output2();
+			var countries = input.Data.GetFlat().Select(c => c.Country).Distinct().ToList();
+			countries.ForEach(c => o.NrOfNeighbors.Add(c, 0));
+
+			input.Data.ExecuteOnAll((value, row, col) =>
+									{
+										if (row > 1)
+										{
+											var above = input.Data.GetAbove(row, col);
+											if (above.Country != value.Country)
+												o.NrOfNeighbors[value.Country]++;
+										}
+									});
 
 			return o;
 		}
--- Test/Solver2Test.cs
+++ Test/Solver2Test.cs
@@ -7,18 +7,18 @@ namespace Test {
 	public class Solver2Test
 	{
 		[Test]
-		[TestCase("Level1/level2_example.in")]
-		[TestCase("Level1/level2_1.in")]
-		[TestCase("Level1/level2_2.in")]
-		[TestCase("Level1/level2_3.in")]
-		[TestCase("Level1/level2_4.in")]
-		[TestCase("Level1/level2_5.in")]
+		[TestCase("Level2/level2_example.in")]
+		[TestCase("Level2/level2_1.in")]
+		[TestCase("Level2/level2_2.in")]
+		[TestCase("Level2/level2_3.in")]
+		[TestCase("Level2/level2_4.in")]
+		[TestCase("Level2/level2_5.in")]
 		public void T1(string file)
 		{
 			var fileInput = file.Read();
-			var input = new TerrainInput1().Parse(fileInput);
+			var input = new TerrainInput2().Parse(fileInput);
 
-			var solution = new Solver1().Solve(input);
+			var solution = new Solver2().Solve(input);
 
 			file.Write(solution.ToString());
 			Assert.Pass();
