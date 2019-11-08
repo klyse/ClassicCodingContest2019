6308eff524c9862f05a1c5be2f04e0f73be06ebe
Volkmar Rigo <volkmar@rigo.bz>
2019-11-08 16:11:32 +0100

    Solver2 v

---
+++ Solver/Algorithms/Solver2v.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Solver.Base;
+using Solver.Model;
+
+namespace Solver.Algorithms
+{
+    public class Solver2v : ISolver<Output2, TerrainInput2>
+    {
+        public Output2 Solve(TerrainInput2 input)
+        {
+            var o = new Output2();
+
+            var countries = input.Matrix.GetFlat().Select(c => c.Country).Distinct().ToList();
+            countries.ForEach(c => o.NrOfNeighbors.Add(c, 0));
+
+            for (int row = 0; row < input.Matrix.Rows; row++)
+                for (int col = 0; col < input.Matrix.Columns; col++)
+                {
+                    var country = input.Matrix[row, col].Country;
+                    var found = 0;
+                    if (col > 0 && input.Matrix[row, col - 1].Country != country)
+                        found++;
+                    if (row > 0 && input.Matrix[row - 1, col].Country != country)
+                        found++;
+                    if (col < input.Matrix.Columns - 1 && input.Matrix[row, col + 1].Country != country)
+                        found++;
+                    if (row < input.Matrix.Rows - 1 && input.Matrix[row +1, col].Country != country)
+                        found++;
+
+                    if (found > 0)
+                        o.NrOfNeighbors[country] = o.NrOfNeighbors[country] + found;
+                }
+
+            return o;
+        }
+    }
+}
---
+++ Test/Solver2vTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using Solver;
+using Solver.Algorithms;
+using Solver.Model;
+
+namespace Test {
+	public class Solver2vTest
+	{
+		[Test]
+		[TestCase("Level2/level2_example.in")]
+		[TestCase("Level2/level2_1.in")]
+		[TestCase("Level2/level2_2.in")]
+		[TestCase("Level2/level2_3.in")]
+		[TestCase("Level2/level2_4.in")]
+		[TestCase("Level2/level2_5.in")]
+		public void T2(string file)
+		{
+			var fileInput = file.Read();
+			var input = new TerrainInput2().Parse(fileInput);
+
+			var solution = new Solver2().Solve(input);
+
+			file.Write(solution.ToString());
+			Assert.Pass();
+		}
+	}
+}
