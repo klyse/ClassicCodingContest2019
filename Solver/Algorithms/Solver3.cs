eb5efb80bc60ac692e9e6cc51552965d48a62346
Volkmar Rigo <volkmar@rigo.bz>
2019-11-08 16:18:22 +0100

    Copy of Solver 2

---
+++ Solver/Algorithms/Solver3.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Solver.Base;
+using Solver.Model;
+
+namespace Solver.Algorithms
+{
+	public class Solver3 : ISolver<Output3, TerrainInput3>
+	{
+		public Output3 Solve(TerrainInput3 input)
+		{
+			var o = new Output3();
+			var countries = input.Matrix.GetFlat().Select(c => c.Country).Distinct().ToList();
+			countries.ForEach(c => o.NrOfNeighbors.Add(c, 0));
+
+			input.Matrix.ExecuteOnAll((value, row, col) =>
+									  {
+										  var adjVal = new List<TerrainInput3.Cell>();
+
+										  if (row > 0)
+											  adjVal.Add(input.Matrix.GetAbove(row, col));
+
+										  if (row < input.Matrix.Rows - 1)
+											  adjVal.Add(input.Matrix.GetBelow(row, col));
+
+										  if (col > 0)
+											  adjVal.Add(input.Matrix.GetLeft(row, col));
+
+										  if (col < input.Matrix.Columns - 1)
+											  adjVal.Add(input.Matrix.GetRight(row, col));
+
+										  if (adjVal.Count < 4)
+										  {
+											  o.NrOfNeighbors[value.Country]++;
+											  return;
+										  }
+
+										  foreach (var v in adjVal)
+										  {
+											  if (v.Country != value.Country)
+											  {
+												  o.NrOfNeighbors[value.Country]++;
+												  break;
+											  }
+										  }
+									  });
+
+			return o;
+		}
+	}
+}
---
+++ Solver/Model/Output3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solver.Model
+{
+	public class Output3
+	{
+		public Dictionary<int, int> NrOfNeighbors { get; set; } = new Dictionary<int, int>();
+
+		public override string ToString()
+		{
+			return string.Join(Environment.NewLine, NrOfNeighbors.OrderBy(c => c.Key).Select(q => q.Value));
+		}
+	}
+}
