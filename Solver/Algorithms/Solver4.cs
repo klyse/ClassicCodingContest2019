0c42ed55254dcf164743b8a56ffd75561f9a17a2
Unknown <Klaus.Pruenster@gmail.com>
2019-11-08 18:25:56 +0100

    added slope

--- Solver/Algorithms/Solver4.cs
+++ Solver/Algorithms/Solver4.cs
@@ -11,37 +11,13 @@ namespace Solver.Algorithms
 		public Output4 Solve(TerrainInput4 input)
 		{
 			var o = new Output4();
-            /*
-			var countries = input.Matrix.GetFlat().Select(c => c.Country).Distinct().ToList();
-			countries.ForEach(c => o.Capital.Add(c, new Point { X = 0, Y = 0 }));
 
-			foreach (var country in countries)
+			foreach (var ray in input.Data)
 			{
-				var coords = new List<Point>();
-
-				input.Matrix.ExecuteOnAll((value, row, col) =>
-										  {
-											  if (value.Country == country)
-												  coords.Add(new Point { X = col, Y = row });
-										  });
-
-				var location = new Point(coords.Select(q => q.X).Sum() / coords.Count, coords.Select(q => q.Y).Sum() / coords.Count);
-
-				if (input.IsBorder(location) || input.IsOutside(location, country))
-				{
-					// Get all inside country cells
-					var temp = coords
-							   .Where(q => !input.IsBorder(q))
-							   .Select(q => new { Location = q, Distance = input.Distance(location, q) })
-							   .OrderBy(q => q.Distance).ThenBy(q => q.Location.Y).ThenBy(q => q.Location.X)
-							   .ToList();
-
-					location = temp.Select(q => q.Location).First();
-				}
-
-				o.Capital[country] = location;
+				var m = (ray.Start.Y - (ray.Start.Y + ray.Direction.Y)) / (ray.Start.X - (ray.Start.X + ray.Direction.X));
 			}
-            */
+
+
 			return o;
 		}
 	}
