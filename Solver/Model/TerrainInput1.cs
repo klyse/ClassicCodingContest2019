541625616b11d68bfcea737f5cfe214c162d09bc
Volkmar Rigo <volkmar@rigo.bz>
2019-11-08 15:09:45 +0100

    First Version... only Col and Row, no data

---
+++ Solver/Model/TerrainInput1.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Solver.Base;
+using NeoMatrix;
+
+namespace Solver.Model
+{
+	public class TerrainInput1 : IInput<TerrainInput1>
+	{
+        public Matrix<int> Data;
+	
+		public TerrainInput1 Parse(string[] values)
+		{
+			var val = values.First().Split(' ').Select(q => int.Parse(q)).ToList();
+
+            Data = new Matrix<int>(val[1], val[0]);
+
+			return this;
+		}
+	}
+}
